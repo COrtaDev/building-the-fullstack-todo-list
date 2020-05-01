c8e0a0e3075976d9090e63571e1aa49686c49b07
Conrad Orta <conradorta@gmail.com>
2020-05-01 16:01:18 -0400

    completed merge-catagory-spec.js

--- test/merge-categories-spec.js
+++ test/merge-categories-spec.js
@@ -12,16 +12,57 @@ describe("mergeCategories()", () => {
     `;
 
     it("should return no <li>s for no categories", () => {
+      //Arrange
+      const catagories = [];
+
+      //Act
+      const result = mergeCategories(template, catagories, 'li');
+
+      //Assert
+      expect(result).to.have.string('<div>');
+      expect(result).to.have.string("</div>");
+      expect(result).to.have.string("<ul>");
+      expect(result).to.have.string("</ul>");
+      expect(result).to.not.have.string("<li>");
+      expect(result).to.not.have.string("</li>");
     });
 
     it("should return a single <li> for one category", () => {
-      expect.fail('please write this test');
+      //Arrange
+      const catagories = ['testing-testing-1-2-3'];
+
+      //Act
+      const result = mergeCategories(template, catagories, 'li');
+
+      //Assert
+      expect(result).to.have.string('<div>');
+      expect(result).to.have.string('</div>');
+      expect(result).to.have.string('<ul>');
+      expect(result).to.have.string('</ul>');
+      expect(result).to.have.string(`<li>${catagories}</li>`);
+      expect(result).to.not.have.string('<!-- Content here -->');
     });
 
     it("should return an <li> for each category", () => {
-      expect.fail('please write this test');
+      //Arrange
+      const catagories = ['testes', 'testies', 'testings', 'tests-Ez'];
+
+      //Act
+      const result = mergeCategories(template, catagories, 'li');
+
+      //Assert
+      expect(result).to.have.string('<div>');
+      expect(result).to.have.string('</div>');
+      expect(result).to.have.string('<ul>');
+      expect(result).to.have.string('</ul>');
+      expect(result).to.have.string(`<li>${catagories[0]}</li>`);
+      expect(result).to.have.string(`<li>${catagories[1]}</li>`);
+      expect(result).to.have.string(`<li>${catagories[2]}</li>`);
+      expect(result).to.have.string(`<li>${catagories[3]}</li>`);
+      expect(result).to.not.have.string('<!-- Content here -->');
     });
   });
+
 
   context("using <option> tags", () => {
     const template = `
@@ -33,15 +74,54 @@ describe("mergeCategories()", () => {
     `;
 
     it("should return no <option>s for no categories", () => {
-      expect.fail('please write this test');
+      //Arrange
+      const catagories = [];
+
+      //Act
+      const result = mergeCategories(template, catagories, 'options');
+
+      //Assert
+      expect(result).to.include('<div>');
+      expect(result).to.include('</div>');
+      expect(result).to.include('<select>');
+      expect(result).to.include('</select>');
+      expect(result).to.not.include('<options>');
+      expect(result).to.not.include('</options>');
     });
 
     it("should return a single <option> for one category", () => {
-      expect.fail('please write this test');
+      //Arrange
+      const catagories = ['a stringy poo, a stinky poo...'];
+
+      //Act
+      const result = mergeCategories(template, catagories, 'options');
+
+      //Assert
+      expect(result).to.include('<div>');
+      expect(result).to.include('</div>');
+      expect(result).to.include('<select>');
+      expect(result).to.include('</select>');
+      expect(result).to.include(`<options>${catagories}</options>`);
+      expect(result).to.not.include('<!-- Content here -->');
     });
 
     it("should return an <option> for each category", () => {
-      expect.fail('please write this test');
+      //Arrange
+      const catagories = ['turkey', 'spurkey', 'jerkey', 'hellurkey'];
+
+      //Act
+      const result = mergeCategories(template, catagories, 'options');
+
+      //Assert
+      expect(result).to.include('<div>');
+      expect(result).to.include('</div>');
+      expect(result).to.include('<select>');
+      expect(result).to.include('</select>');
+      expect(result).to.include(`<options>${catagories[0]}</options>`);
+      expect(result).to.include(`<options>${catagories[1]}</options>`);
+      expect(result).to.include(`<options>${catagories[2]}</options>`);
+      expect(result).to.include(`<options>${catagories[3]}</options>`);
+      expect(result).to.not.include('<!-- Content here -->');
     });
   });
 });
